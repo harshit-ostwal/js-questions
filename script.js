bad3073394a7b147d008f817f33531fb17ac936b
Harshit Jain <harshitostwal1234@gmail.com>
2026-02-07 05:35:24 +0530

    Add new JavaScript questions and update question display in the quiz interface

--- script.js
+++ script.js
@@ -153,6 +153,164 @@ function question20() {
   print("Pushing New Array Value : " + JSON.stringify(arr));
 }
 
+function question21() {
+  print("Printing Value From 1 to 50 using For.");
+  for (let i = 1; i <= 50; i++) {
+    print(i);
+  }
+}
+
+function question22() {
+  let sum = 0;
+  let i = 1;
+
+  while (i <= 10) {
+    sum += i;
+    i++;
+  }
+  print("Your Sum Value From 1 To 10 is " + sum);
+}
+
+function question23() {
+  const value = "JavaScript";
+  print("Printing characters using for...of");
+
+  for (let char of value) {
+    print(char);
+  }
+}
+
+function question24() {
+  let i = 1;
+  print("Printing ODD Numbers Between 1 to 20.");
+  while (i <= 20) {
+    i++;
+    if (i % 2 === 0) {
+      continue;
+    }
+    print(i);
+  }
+}
+
+function question25() {
+  let i = 5;
+  print("Printing Value From 5 To 1. Using Do While");
+  do {
+    print(i);
+    i--;
+  } while (i >= 1);
+}
+
+function question26() {
+  let res = 1;
+  for (let i = 1; i <= 5; i++) {
+    res = res * i;
+  }
+  print("5 Factorial is : " + res);
+}
+
+function question29() {
+  print("Printing Numbers From 1 to 100 Divisible By 5 Using While.");
+  let i = 1;
+
+  while (i <= 100) {
+    if (i % 5 === 0) {
+      print(i);
+    }
+    i++;
+  }
+}
+
+function question30() {
+  const object = {
+    name: "Harshit",
+    age: "21",
+  };
+
+  print("Printing Object Before For in");
+  print(JSON.stringify(object));
+
+  print("Printing Object { name, age } & Print Keys Using For in");
+  for (let key in object) {
+    print(key);
+  }
+}
+
+function question31() {
+  const number = Number(prompt("Enter a Number"));
+  print("Check Your Number is Postive / Negative - " + number);
+  if (number > 0) {
+    print("Positive");
+  } else {
+    print("Negative");
+  }
+}
+
+function question32() {
+  const age = Number(prompt("Enter your Age"));
+
+  print("Check Person's Age They're Minor / Major");
+  if (age >= 18) {
+    print("Adult");
+  } else {
+    print("Minor");
+  }
+}
+
+function question33() {
+  const value = Number(prompt("Enter a Value"));
+  print("Check Even or Odd");
+
+  if (value % 2 == 0) {
+    print("Even, Your Value is " + value);
+  } else {
+    print("Odd Your Value is " + value);
+  }
+}
+
+function question34() {
+  const num1 = Number(prompt("Enter a Number 1"));
+  const num2 = Number(prompt("Enter a Number 2"));
+
+  print("Finding Greater Value Between 2 Numbers.");
+  print("Your Number 1 Value is " + num1);
+  print("Your Number 2 Value is " + num2);
+
+  if (num1 > num2) {
+    print("Number 1 Value is Greater Than Number 2");
+  } else if (num1 == num2) {
+    print("Number 1 & Number 2 Value is Equal");
+  } else {
+    print("Number 2 Value is Greater Than Number 1");
+  }
+}
+
+function question35() {
+  const score = Number(prompt("Enter your Score"));
+
+  print("Score Grading");
+  print("Your Score is : " + score);
+
+  if (score >= 90) {
+    print("Your Score Grading is : Excellent");
+  } else if (score >= 70) {
+    print("Your Score Grading is : Good");
+  } else {
+    print("Your Score Grading is : Needs Improvement");
+  }
+}
+
+function question36() {
+  const year = Number(prompt("Enter a Year"));
+  print("Check Leap Year");
+
+  if (year % 100 === 0 ? year % 400 === 0 : year % 4 === 0) {
+    print("Leap Year");
+  } else {
+    print("Not a Leap Year");
+  }
+}
+
 const questions = [
   question1,
   question2,
@@ -174,6 +332,34 @@ const questions = [
   question18,
   question19,
   question20,
+  question21,
+  question22,
+  question23,
+  question24,
+  question25,
+  question26,
+  question29,
+  question30,
+  question31,
+  question32,
+  question33,
+  question34,
+  question35,
+  question36,
+  // question37,
+  // question38,
+  // question39,
+  // question40,
+  // question41,
+  // question42,
+  // question43,
+  // question44,
+  // question45,
+  // question46,
+  // question47,
+  // question48,
+  // question49,
+  // question50,
 ];
 
 questionButtons.forEach((btn, index) => {
